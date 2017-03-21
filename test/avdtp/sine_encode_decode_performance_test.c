3677d90edd35b7e28dc3f38bc8ed28f071c2233c
Matthias Ringwald <matthias@ringwald.ch>
2017-03-21 12:27:18 +0100

    avdtp: use static sine table to avoid maths library

--- test/avdtp/sine_encode_decode_performance_test.c
+++ test/avdtp/sine_encode_decode_performance_test.c
@@ -46,18 +46,40 @@
 #include "avdtp_source.h"
 #include "stdin_support.h"
 
+// #define GENERATE_TABLE
+
 #define NUM_CHANNELS        2
 #define SAMPLE_RATE         44100
 #define BYTES_PER_AUDIO_SAMPLE   (2*NUM_CHANNELS)
 #define LATENCY             300 // ms
 
+#define TABLE_SIZE_441HZ    100
+
+#ifdef GENERATE_TABLE
 #ifndef M_PI
 #define M_PI  3.14159265
 #endif
-#define TABLE_SIZE_441HZ   100
+
+static int16_t sine_table[TABLE_SIZE_441HZ];
+
+#else
+
+static int16_t sine_table[TABLE_SIZE_441HZ] = {
+     0,   2057,   4106,   6139,   8148,  10125,  12062,  13951,  15785,  17557,
+ 19259,  20886,  22430,  23886,  25247,  26509,  27666,  28713,  29648,  30465,
+ 31163,  31737,  32186,  32508,  32702,  32767,  32702,  32508,  32186,  31737,
+ 31163,  30465,  29648,  28713,  27666,  26509,  25247,  23886,  22430,  20886,
+ 19259,  17557,  15785,  13951,  12062,  10125,   8148,   6139,   4106,   2057,
+     0,  -2057,  -4106,  -6139,  -8148, -10125, -12062, -13951, -15785, -17557,
+-19259, -20886, -22430, -23886, -25247, -26509, -27666, -28713, -29648, -30465,
+-31163, -31737, -32186, -32508, -32702, -32767, -32702, -32508, -32186, -31737,
+-31163, -30465, -29648, -28713, -27666, -26509, -25247, -23886, -22430, -20886,
+-19259, -17557, -15785, -13951, -12062, -10125,  -8148,  -6139,  -4106,  -2057,
+};
+
+#endif
 
 typedef struct {
-    int16_t source[TABLE_SIZE_441HZ];
     int left_phase;
     int right_phase;
 } paTestData;
@@ -84,8 +106,8 @@ static void fill_sine_frame(void *userData, int num_samples_to_write){
     int offset = 0;
     while (count < num_samples_to_write){
         uint8_t write_data[BYTES_PER_AUDIO_SAMPLE];
-        *(int16_t*)&write_data[0] = data->source[data->left_phase];
-        *(int16_t*)&write_data[2] = data->source[data->right_phase];
+        *(int16_t*)&write_data[0] = sine_table[data->left_phase];
+        *(int16_t*)&write_data[2] = sine_table[data->right_phase];
         
         memcpy(pcm_frame+offset, write_data, BYTES_PER_AUDIO_SAMPLE);
         offset += BYTES_PER_AUDIO_SAMPLE;
@@ -110,9 +132,20 @@ int btstack_main(int argc, const char * argv[]){
                     
     /* initialise sinusoidal wavetable */
     int i;
+#ifdef GENERATE_TABLE
+    printf("static int16_t sine_table[TABLE_SIZE_441HZ] = {\n");
     for (i=0; i<TABLE_SIZE_441HZ; i++){ 
-        sin_data.source[i] = sin(((double)i/(double)TABLE_SIZE_441HZ) * M_PI * 2.)*32767;
+        sine_table[i] = sin(((double)i/(double)TABLE_SIZE_441HZ) * M_PI * 2.)*32767;
+        if ((i % 10) == 0){
+            printf("    ");
+        }
+        printf("% 6d, ", sine_table[i]);
+        if ((i % 10) == 9) {
+            printf("\n");
+        }
     }
+    printf(")};\n");
+#endif
     sin_data.left_phase = sin_data.right_phase = 0;
     
     btstack_sbc_decoder_init(&sbc_decoder_state, mode, handle_pcm_data, NULL);
